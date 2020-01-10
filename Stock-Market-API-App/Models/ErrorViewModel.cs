5d7fe67b801f4aba45e929a6b507744cdf09f904
Mitch Dorsey <mitchellrdorsey@gmail.com>
2020-01-10 13:56:33 -0500

    First commit, fresh .Net Core Web App Solution

---
+++ Stock-Market-API-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stock_Market_API_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
